535e36bb8f5e7c6775fbb098d0b5aebedfd79ec0
Drawde <60484184+edwardtimoty@users.noreply.github.com>
2021-04-22 23:20:23 +0700

    latihan DOM (mengubah warna menggunakan button dan pointer)
    22/4/2021

---
+++ 4. Latihan DOM 1/script.js
@@ -0,0 +1,61 @@
+const tombolUbahWarna = document.getElementById('tombolUbahWarna');
+// const body = document.getElementsByTagName('body')[0];
+
+tombolUbahWarna.onclick = function() {
+    // document.body.style.backgroundColor = 'lightblue';
+    // document.body.setAttribute('class', 'biru-muda');
+    document.body.classList.toggle('biru-muda');
+}
+
+const tAcakWarna = document.createElement('button');
+const teksTombol = document.createTextNode('Acak Warna');
+tAcakWarna.appendChild(teksTombol);
+tAcakWarna.setAttribute('type', 'button');
+tombolUbahWarna.after(tAcakWarna);
+
+tAcakWarna.addEventListener('click', function(){
+    // Math.random = membuat angka acak, Math.round = membulatkan angka
+    const r = Math.round(Math.random() * 255 + 1);
+    const g = Math.round(Math.random() * 255 + 1);
+    const b = Math.round(Math.random() * 255 + 1);
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g+','+ b +')';
+});
+
+
+const sMerah = document.querySelector('input[name=sMerah]');
+const sHijau = document.querySelector('input[name=sHijau]');
+const sBiru = document.querySelector('input[name=sBiru]');
+
+sMerah.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+sHijau.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+sBiru.addEventListener('input', function() {
+    const r = sMerah.value;
+    const g = sHijau.value;
+    const b = sBiru.value;
+    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
+});
+
+
+document.body.addEventListener('mousemove', function(event) {
+    // posisi mouse
+    // event.clientX
+    // ukuran browser
+    // window.innerWidth
+
+    const xPos = Math.round((event.clientX / window.innerWidth) * 255);
+    const yPos = Math.round((event.clientY / window.innerHeight) * 255);
+    
+    document.body.style.backgroundColor = 'rgb('+ xPos +','+ yPos +',100)';
+});
