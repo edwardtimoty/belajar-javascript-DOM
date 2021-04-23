28b0703f0d6875ba025b774a2d5b08fe6fc59982
Drawde <60484184+edwardtimoty@users.noreply.github.com>
2021-04-23 16:21:15 +0700

    belajar DOM Traversal
    23/4/2021

---
+++ 6. DOM Traversal/script.js
@@ -0,0 +1,31 @@
+// Event Handling
+// const close = document.querySelector('.close');
+// const card = document.querySelector('.card');
+
+// close.addEventListener('click', function(){
+//     card.style.display = 'none';
+// });
+
+// DOM Traversal
+const close = document.querySelectorAll('.close');
+
+// cara 1 :
+// for(let i = 0; i < close.length; i++) 
+// {
+//     close[i].addEventListener('click', function(event) {
+//         // close[i].parentElement.style.display = 'none';
+//         // console.log(event.target);
+//         event.target.parentElement.style.display = 'none';
+
+//     })
+// }
+
+// cara 2 :
+close.forEach(function(el){
+    el.addEventListener('click', function(e) {
+        e.target.parentElement.style.display = 'none';
+    });
+});
+
+const nama = document.querySelector('.nama');
+console.log(nama.previousElementSibling);
